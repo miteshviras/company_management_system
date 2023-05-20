7f70d4d651c178d24c0728027d4493445d546ad1
bhavik kumar desai <bhavik@alitainfotech.com>
2023-05-20 12:08:42 +0530

    migrations and laravel breeze added

---
+++ app/Http/Controllers/UserLeaveController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserLeave;
+use Illuminate\Http\Request;
+
+class UserLeaveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserLeave $userLeave)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserLeave $userLeave)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserLeave $userLeave)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserLeave $userLeave)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->create_admin();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+    }
+
+    private function create_admin(string $email = 'admin@example.com',string $password = "admin@123"): void
+    {
+        $userExist = User::where('email',$email)->first();
+        if(empty($userExist))
+        User::create([
+            'name' => 'admin',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'is_admin' => 1
+        ]);
     }
 }
